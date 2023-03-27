f607a5e1dbadcc6d039398acf0c3b6451151ca34
Derick <103431593+olotuderick@users.noreply.github.com>
2023-03-27 09:07:13 +0300

    init

---
+++ tweets/views.py
@@ -0,0 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import TweetSerializer
+from .models import Tweet
+
+
+@api_view(["GET"])
+def all_tweets(request):
+    tweets = Tweet.objects.all()
+    serializer = TweetSerializer(tweets, many=True)
+    return Response(serializer.data)
